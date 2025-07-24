2806a66eb3820e65c715a3082254bbd7bf84f106
Andrei Mazniak <andreymaznyak@gmail.com>
2025-07-24 11:40:19 +0200

    Update test drive date to 2025 and footer copyright year to 2025

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -92,7 +92,7 @@ const Footer = () => {
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center">
           <p className="text-gray-400">
-            © 2024 Cool Cars Amsterdam. All rights reserved.
+            © 2025 Cool Cars Amsterdam. All rights reserved.
           </p>
         </div>
       </div>
